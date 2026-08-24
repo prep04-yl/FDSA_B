f6c29ef61c915bc04a0b8995fb1f4e9191edd581
prep04-yl <preyp0412@gmail.com>
2026-08-24 13:41:23 +0530

    Optimize sorting with counting sort algorithm
    Replaced the sorting algorithm with a counting sort implementation for better efficiency.

--- Practical 3/practical3problem2.cpp
+++ Practical 3/practical3problem2.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 void sort_codes(int (&arr)[], int n)
 {
-    for(int i=0; i<n-1; i++)
+    int count[3]={0};
+
+    for(int i=0; i<n; i++)
     {
-        for(int j=i+1; j<n; j++)
+        count[arr[i]]++;
+    }
+
+    int k=0;
+    for(int i=0; i<3; i++)
+    {
+        while(count[i]--)
         {
-            if(arr[i]>arr[j])
-                swap(arr[i], arr[j]);
+            arr[k++]=i;
         }
     }
 }
@@ -17,7 +24,12 @@ int main()
 {
     int arr[]={1,2,1,0,2,0,2,0,1,1,2,0};
     int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Original: ";
+    for(int i=0; i<n;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
     sort_codes(arr, n);
+    cout<<"Sorted list: ";
     for(int i=0; i<n; i++)
     {
         cout<<arr[i]<<" ";
